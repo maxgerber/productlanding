737e0a8f4080f6f0822d6e4bf7fb28d410315b0a
Max Gerber <mg14859@my.bristol.ac.uk>
2017-05-11 22:53:34 +0100

    Navbar toggle working,image issues

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 window.addEventListener("DOMContentLoaded", function() {
-//Add most of your code here!
+    var navWidth = !!document.getElementsByTagName("nav")[0].style.width;
+
+    function openNav() {
+        document.getElementsByTagName("nav")[0].style.width = "20%";
+        document.getElementsByTagName("body")[0].style.paddingLeft = "20%";
+        document.getElementsByTagName("main")[0].style.width = "58%";
+        // document.getElementsByTagName("header")[0].style.width = "80%";
+    //    document.getElementsByTagName("figure")[0].style.width = "80%";
+        navWidth = "20%";
+    }
+
+    function closeNav() {
+        document.getElementsByTagName("nav")[0].style.width = 0;
+        document.getElementsByTagName("body")[0].style.paddingLeft = 0;
+        document.getElementsByTagName("main")[0].style.width = "78%";
+           document.getElementsByTagName("figure")[0].style.width = "100%";
+
+        navWidth = 0;
+    }
+
+    function toggle() {
+        if ( navWidth == 0 ) {
+            openNav();
+        } else {
+            closeNav();
+        }
+    }
+
+    document.getElementById("open").addEventListener("click", toggle);
 });
